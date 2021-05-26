9b8ecaced16af0ef36190875f6ff66dd3ef7ac3e
giothcode <gio666nb@gmail.com>
2021-05-26 11:25:02 -0500

    Add tailwindcss

--- front-page.php
+++ front-page.php
@@ -1,3 +1,18 @@
 <?php get_header(); ?>
 
+<main>
+    <div class="flex items-center space-x-10">
+        <div class="w-1/12">
+            <img 
+                class="rounded-full"
+                src="<?php echo get_template_directory_uri() ?>/assets/img/giothcode.jfif" 
+                alt="imagen de perfil de Gio"
+            >
+        </div>
+        <div class="w-6/12">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem iure pariatur autem repudiandae, a dolores, temporibus ipsum vero ut nulla distinctio rem consequatur? Laboriosam suscipit, ullam placeat possimus perferendis veniam? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam, debitis quisquam corrupti animi quos quae explicabo nostrum. Aut recusandae atque porro ipsam cumque minus quod, cum ipsa repudiandae voluptas officiis?</p>
+        </div>
+    </div>
+</main>
+
 <?php get_footer(); ?>
